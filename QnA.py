81fe30876136c954c5958c27e793407d00fa4829
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-18 14:15:52 +0530

    Update QnA.py

--- QnA.py
+++ QnA.py
@@ -89,4 +89,4 @@ with st.form("myform"):
     if not genai_api_key:
         st.info("Please add your GenAI API key & GenAI API URL to continue.")
     elif submitted:
-    return answer
+        return answer
