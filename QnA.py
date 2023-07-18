0a435f0c26f6e6b9e2d2edbeb416c8cfcbe7cced
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-18 21:02:41 +0530

    Update QnA.py

--- QnA.py
+++ QnA.py
@@ -21,29 +21,31 @@ genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
 chunk_size = st.sidebar.text_input("Select Chunk size", type="default")
 chunk_overlap = st.sidebar.text_input("Select Chunk overlap", type="default")
 
-uploaded_file = st.file_uploader("Choose PDF file", type="pdf", accept_multiple_files=False)
-
-pdf_reader = PdfReader(uploaded_file)
-for i, page in enumerate(pdf_reader.pages):
-    text = page.extract_text()
-    if text:
-        raw_text += text
-        text_splitter = CharacterTextSplitter(
-            separator="\n", # line break
-            chunk_size = chunk_size,
-            chunk_overlap = chunk_overlap,  
-            length_function=len,
-        )
-
-texts = text_splitter.split_text(raw_text)
-embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large",model_kwargs={"device": "cpu"})
-embeddings = embeddings
-docsearch = Chroma.from_documents(texts, embeddings)
-creds = Credentials(api_key=genai_api_key, api_endpoint=genai_api_url)
-params= GenerateParams(decoding_method="sample", temperature=0.7, max_new_tokens=400, min_new_tokens=10, repetition_penalty=2)
-llm=LangChainInterface(model=ModelType.FLAN_T5_11B, params=params, credentials=creds)
-qa=RetrievalQA.from_chain_type(llm=llm, chain_type="stuff",retriever=docsearch.as_retriever())
-
-query = st.text_input("Ask a question or give an instruction")
-answer = qa.run(query)
-st.write(answer)
+uploaded_files = st.file_uploader("Choose PDF files", type="pdf", accept_multiple_files=True)
+if uploaded_files:
+    raw_text = ''
+    # Loop through each uploaded file
+    for uploaded_file in uploaded_files:
+        pdf_reader = PdfReader(uploaded_file)
+        for i, page in enumerate(pdf_reader.pages):
+            text = page.extract_text()
+            if text:
+                raw_text += text
+                text_splitter = CharacterTextSplitter(
+                    separator="\n", # line break
+                    chunk_size = chunk_size,
+                    chunk_overlap = chunk_overlap,  
+                    length_function=len,
+                )
+                
+                texts = text_splitter.split_text(raw_text)
+                embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large",model_kwargs={"device": "cpu"})
+                embeddings = embeddings
+                docsearch = Chroma.from_documents(texts, embeddings)
+                creds = Credentials(api_key=genai_api_key, api_endpoint=genai_api_url)
+                params= GenerateParams(decoding_method="sample", temperature=0.7, max_new_tokens=400, min_new_tokens=10, repetition_penalty=2)llm=LangChainInterface(model=ModelType.FLAN_T5_11B, params=params, credentials=creds)
+                qa=RetrievalQA.from_chain_type(llm=llm, chain_type="stuff",retriever=docsearch.as_retriever())
+                
+                query = st.text_input("Ask a question or give an instruction")
+                answer = qa.run(query)
+                st.write(answer)
