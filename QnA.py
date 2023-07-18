c9cae96e4f84865199a8d832ba3dfe8fa0ac83b7
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-18 19:52:26 +0530

    Update QnA.py

--- QnA.py
+++ QnA.py
@@ -11,8 +11,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain import OpenAI, PromptTemplate, LLMChain
 from langchain.chains import RetrievalQA
 from langchain.vectorstores import Chroma
-import base64
-import fitz
+from PyPDF2 import PdfReader
 
 st.title("Retrieval Augmented Generation App")
 st.caption("This app was developed by Sharath Kumar RK, Ecosystem Engineering Watsonx team")
@@ -22,15 +21,30 @@ genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
 chunk_size = st.sidebar.text_input("Select Chunk size", type="default")
 chunk_overlap = st.sidebar.text_input("Select Chunk overlap", type="default")
 
-
-uploaded = st.file_uploader(label="Please browse for a pdf file", type="pdf")
-if uploaded is None:
-    st.stop()
-
-base64_pdf = base64.b64encode(uploaded.read()).decode("utf-8")
+uploaded_files = st.file_uploader("Choose PDF files", type="pdf", accept_multiple_files=True)
+if uploaded_files:
+    raw_text = ''
+    # Loop through each uploaded file
+    for uploaded_file in uploaded_files:
+        # Read the PDF
+        pdf_reader = PdfReader(uploaded_file)
+                # Loop through each page in the PDF
+        for i, page in enumerate(pdf_reader.pages):
     
-splitter=CharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
-chunked_docs=splitter.split_documents(base64_pdf)
+            # Extract the text from the page
+            text = page.extract_text()
+        
+            # If there is text, add it to the raw text
+            if text:
+              raw_text += text
+                text_splitter = CharacterTextSplitter(
+                    separator="\n", # line break
+                    chunk_size = 1000,
+                    chunk_overlap = 200,  
+                    length_function=len,
+                )
+
+texts = text_splitter.split_text(raw_text)
 embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large",model_kwargs={"device": "cpu"})
 embeddings = embeddings
 docsearch = Chroma.from_documents(chunked_docs, embeddings)
@@ -38,13 +52,6 @@ creds = Credentials(api_key=genai_api_key, api_endpoint=genai_api_url)
 params= GenerateParams(decoding_method="sample", temperature=0.7, max_new_tokens=400, min_new_tokens=10, repetition_penalty=2)
 llm=LangChainInterface(model=ModelType.FLAN_T5_11B, params=params, credentials=creds)
 qa=RetrievalQA.from_chain_type(llm=llm, chain_type="stuff",retriever=docsearch.as_retriever())
-
-with st.form("myform"):
-    question = st.text_input("Type your question:", value="", placeholder="")
-    submitted = st.form_submit_button("Submit")
-    if submitted and genai_api_key.startswith('pak-'):
-        with st.spinner('Working on it...'):
-            if not genai_api_key:
-                st.info("Please add your GenAI API key & GenAI API URL to continue.")
-            elif submitted:      
-                qa.run(question)
+query = st.text_input("Ask a question or give an instruction")
+answer = qa.run(query)
+st.write(answer)
